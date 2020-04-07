d5b1a9ac5d7bd785cf41dd80ff8601c25a1e92a0
Sang-Yun Oh <syoh@ucsb.edu>
2020-04-07 22:19:12 +0000

    update assignment 0

---
+++ assignments/assignment0/student/tests/q1.py
@@ -0,0 +1,10 @@
+test = {   'hidden': False,
+    'name': 'q1',
+    'points': 10,
+    'suites': [   {   'cases': [   {   'code': '>>> square_num(2)\n4',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ assignments/assignment0/student/tests/q2.py
@@ -0,0 +1,12 @@
+test = {   'hidden': False,
+    'name': 'q2',
+    'points': 10,
+    'suites': [   {   'cases': [   {   'code': '>>> '
+                                               'fruit_by_color.index.to_list()\n'
+                                               '[1, 3, 0, 2]',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
