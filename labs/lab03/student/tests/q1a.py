2c4e00deed683038e183d1822e330c3f6509e229
Sang-Yun Oh <syoh@ucsb.edu>
2020-04-15 03:01:51 +0000

    lab03

---
+++ labs/lab03/student/tests/q1a.py
@@ -0,0 +1,13 @@
+test = {   'hidden': False,
+    'name': 'q1a',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> '
+                                               'calls_by_cvlegend_and_offense["LARCENY", '
+                                               '"THEFT FROM PERSON"] == 24\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ labs/lab03/student/tests/q3a.py
@@ -0,0 +1,22 @@
+test = {   'hidden': False,
+    'name': 'q3a',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> set(calls["Day"]) == '
+                                               "{'Friday', 'Monday', "
+                                               "'Saturday', 'Sunday', "
+                                               "'Thursday', 'Tuesday', "
+                                               "'Wednesday'}\n"
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> list(calls["Day"][:5]) == '
+                                               "['Wednesday', 'Wednesday', "
+                                               "'Friday', 'Tuesday', "
+                                               "'Saturday']\n"
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
